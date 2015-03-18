933a8f1fa38a21e73cb8bd662eddd02b5f9a49a9
delucamarco <delucamarco@users.noreply.github.com>
2015-03-18 10:19:39 +0100

    Basic implementation of heap sort

--- algorithms/main.cpp
+++ algorithms/main.cpp
@@ -1,27 +1,58 @@
 #include <iostream>
 
-inline int parent(int i)
+inline int left(int i) { return i<<1; }
+inline int right(int i) { return (i<<1)+1; }
+
+void max_heapify(int* A, int heap_size, int i)
 {
-    return i>>1;
+    int l=left(i);
+    int r=right(i);
+
+    int largest;
+    if (l<=heap_size && A[l-1]>A[i-1])
+    {
+        largest=l;
+    }
+    else
+    {
+        largest=i;
+    }
+    if (r<=heap_size && A[r-1]>A[largest-1])
+    {
+        largest=r;
+    }
+    if (largest!=i)
+    {
+        int temp=A[i-1];
+        A[i-1]=A[largest-1];
+        A[largest-1]=temp;
+        max_heapify(A, heap_size, largest);
+    }
 }
 
-inline int left(int i)
+void build_max_heap(int* A, int length)
 {
-    return i<<1;
+    int heap_size = length;
+    for (int i=length>>1; i>=1; --i)
+    {
+        max_heapify(A, heap_size, i);
+    }
 }
 
-inline int right(int i)
+void print_array(int* A, int length)
 {
-    return (i<<1)+1;
+    for (int i=0; i<length; ++i)
+        std::cout<<A[i]<<" ";
+    std::cout<<std::endl;
 }
 
 int main()
 {
-    int i = 5;
-    std::cout << "i: " << i
-        << " PARENT(i): " << parent(i)
-        << " LEFT(i): " << left(i)
-        << " RIGHT(i): " << right(i)
-        << std::endl;
+    const int N=10;
+    int A[] = { 4, 1, 3, 2, 16, 9, 10, 14, 8, 7 };
+
+    print_array(A, N);
+    build_max_heap(A, N);
+    print_array(A, N);
     return 0;
 }
